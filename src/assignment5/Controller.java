32b9d841f2f968312552089550c05d9613c61772
thienson-ho <thiensonho@utexas.edu>
2018-04-03 17:51:32 -0500

    Button testing works

--- src/assignment5/Controller.java
+++ src/assignment5/Controller.java
@@ -1,4 +1,21 @@
 package assignment5;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void makeButton(ActionEvent event) {
+        System.out.println("Make Button");
+    }
+
+    public void statsButton(ActionEvent event) {
+        System.out.println("Stats Button");
+    }
+
+    public void seedButton(ActionEvent event) {
+        System.out.println("Seed Button");
+    }
+
+    public void stepButton(ActionEvent event) {
+        System.out.println("Step Button");
+    }
 }
